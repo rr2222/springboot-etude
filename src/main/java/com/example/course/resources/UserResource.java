ff2c415ecb6ef3738aa1cc6fb8fa630b3676b8b0
unknown <ruither.carvalho@gmail.com>
2021-03-24 22:57:53 -0300

    Criada entidade user e criado resource UserResource

---
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.example.course.resources;
+
+import com.example.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
